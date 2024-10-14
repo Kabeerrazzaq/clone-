200f75070943ad7df4df7c390ebfe167ca8a1f54
Abdul Kabeer <161349706+Kabeerrazzaq@users.noreply.github.com>
2024-10-15 01:17:52 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-teal': 'rgba(36, 207, 166, 1)'
+    },
+  },
+  plugins: [],
+},
+};
